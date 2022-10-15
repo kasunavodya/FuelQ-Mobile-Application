5d0cc07d5a1d39b00ce9d70a0b915dae8448c2c8
kasunavodya@gmail.com <kasunavodya@gmail.com>
2022-10-15 19:02:49 +0530

    2022.10.15 - Home, Login and Registration interfaces added

---
+++ app/src/main/java/com/example/fuelq/CustomerRegistration.java
@@ -0,0 +1,13 @@
+package com.example.fuelq;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_registration);
+    }
+}
