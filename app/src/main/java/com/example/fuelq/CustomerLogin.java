8c88db63fde8e6e895b36341b135a4760a35fff0
kasunavodya@gmail.com <kasunavodya@gmail.com>
2022-10-16 17:54:44 +0530

    2022.10.16 - All the interfaces completed

--- app/src/main/java/com/example/fuelq/CustomerLogin.java
+++ app/src/main/java/com/example/fuelq/CustomerLogin.java
@@ -3,6 +3,7 @@ package com.example.fuelq;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CustomerLogin extends AppCompatActivity {
@@ -17,7 +18,13 @@ public class CustomerLogin extends AppCompatActivity {
 
             Intent activityIntent = new Intent(CustomerLogin.this, MainRegistration.class);
             CustomerLogin.this.startActivity(activityIntent);
+        });
 
+        final Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(view -> {
+
+            Intent activityIntent = new Intent(CustomerLogin.this, CustomerHome.class);
+            CustomerLogin.this.startActivity(activityIntent);
         });
     }
 }
--- app/src/main/java/com/example/fuelq/ViewOwnerFuelDetails.java
+++ app/src/main/java/com/example/fuelq/ViewOwnerFuelDetails.java
@@ -12,9 +12,15 @@ public class ViewOwnerFuelDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_owner_fuel_details);
 
-        final ImageView reg1Btn = (ImageView) findViewById(R.id.img_edit);
-        reg1Btn.setOnClickListener(view -> {
+        final ImageView imgEdit = (ImageView) findViewById(R.id.img_edit);
+        imgEdit.setOnClickListener(view -> {
             Intent activityIntent = new Intent(ViewOwnerFuelDetails.this, UpdateFuelDetails.class);
+            ViewOwnerFuelDetails.this.startActivity(activityIntent);
+        });
+
+        final ImageView imgDelete = (ImageView) findViewById(R.id.img_delete);
+        imgDelete.setOnClickListener(view -> {
+            Intent activityIntent = new Intent(ViewOwnerFuelDetails.this, DeleteConfirmation.class);
             ViewOwnerFuelDetails.this.startActivity(activityIntent);
         });
     }
