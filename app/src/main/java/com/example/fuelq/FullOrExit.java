4adb11b389b62ebd38391fc099d3c488b02a2404
tharushikadealwis <it19016108@my.sliit.lk>
2022-10-19 02:07:43 +0530

    get method connected with front end

--- app/src/main/java/com/example/fuelq/FullOrExit.java
+++ app/src/main/java/com/example/fuelq/FullOrExit.java
@@ -2,7 +2,10 @@ package com.example.fuelq;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,4 +30,5 @@ public class FullOrExit extends AppCompatActivity {
             FullOrExit.this.startActivity(activityIntent);
         });
     }
+
 }
