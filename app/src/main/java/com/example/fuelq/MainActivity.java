fcf2e257a581b53daf0e450da0f53c84dcc3aed2
kasunavodya@gmail.com <kasunavodya@gmail.com>
2022-10-19 09:49:38 +0530

    2022.10.19 - files modified

--- app/src/main/java/com/example/fuelq/MainActivity.java
+++ app/src/main/java/com/example/fuelq/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.fuelq;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String URL = "https://localhost:7060/api/Customer";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                response -> Log.e("Rest Response", response.toString()),
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
 
         final TextView TitleView = (TextView) findViewById(R.id.txt_topic);
         TitleView.setOnClickListener(view -> {
